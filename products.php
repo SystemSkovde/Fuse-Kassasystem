142d46c3e58905d93e49ab3f782192273fae0aa6
hedvigandersson99 <hedvig.andersson99@gmail.com>
2026-04-26 14:14:27 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,6 +1,6 @@
 <?php
   $PDO = new PDO("mysql:dbname=Fuse; host=127.0.0.1", "dbftg", "Myrlejon2026!");
-  $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $PDO->setAttribute(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $sql = "SELECT * FROM Products";
 $stmt = $pdo->query($sql);
