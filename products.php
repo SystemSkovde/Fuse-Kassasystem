3a84f9185069bc7717d4e2eca9b2f1651121af3f
Amanda-1111 <laisdaone@gmail.com>
2026-04-26 14:01:47 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,26 +1,15 @@
 <?php
-    $pdo = new PDO(
-            'mysql:dbname=Fuse;host=127.0.0.1;charset=utf8mb4',
-            'dbftg',
-            'Myrlejon2026!',
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-        );
-
-
-if ($pdo->connect_error) {
-    die("Connection failed: " . $pdo->connect_error);
-}
+$pdo = new PDO(
+    'mysql:dbname=Fuse;host=127.0.0.1;charset=utf8mb4',
+    'dbftg',
+    'Myrlejon2026!',
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
 
 $sql = "SELECT * FROM Products";
-$result = $pdo->query($sql);
+$stmt = $pdo->query($sql);
 
-$products = [];
-
-while($row = $result->fetch_assoc()) {
-    $products[] = $row;
-}
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($products);
-
-$pdo->close();
 ?>
