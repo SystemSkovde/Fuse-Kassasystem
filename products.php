a72266e1f12ac5a13e56bf4d2cd24b76dbbf8f85
Amanda-1111 <laisdaone@gmail.com>
2026-04-29 10:40:14 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,9 +1,11 @@
 <?php
 $pdo = new PDO(
-    "mysql:host=127.0.0.1;port=3306;dbname=your_db",
-    "fuse_app",
-    "fuse123"
-);
+            'mysql:dbname=Fuse;host=127.0.0.1;charset=utf8mb4',
+            'dbftg',
+            'Myrlejon2026!',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+
 // Sätt attribut korrekt
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
