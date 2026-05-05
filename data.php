41132010379ceebf68784e26188545fa3a8d5c9e
hedvigandersson99 <hedvig.andersson99@gmail.com>
2026-05-05 15:34:06 +0200

    Update data.php

--- data.php
+++ data.php
@@ -17,14 +17,18 @@ $products = $stmt->fetchAll();
 
 echo json_encode($products);
 
-$data = json_decode(file_get_contents("php://input"), true);
-$cid = $data['cid'];
-$password = $data['password'];
 
-$sql2 = "SELECT * FROM Users where cid = :cid";
-$stmt = $pdo->prepare($sql2);
-$stmt->execute([':cid' => $cid]);
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-echo json_encode($user);
+$cid = $_POST['cid'] ?? null;
+$password = $_POST['password'] ?? null;
+
+if ($cid !== null && $password !== null) {
+    $sql = "SELECT * FROM Users WHERE cid = :cid";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':cid' => $cid]);
+
+    $user = $stmt->fetch();
+
+    echo json_encode($user);
+}
 
 ?>
