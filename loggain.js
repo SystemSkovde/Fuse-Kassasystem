7134b96ba8cd0317ad845f4e4abbbfd2a6413564
Amanda-1111 <laisdaone@gmail.com>
2026-05-05 17:43:19 +0200

    Kopplat

--- loggain.js
+++ loggain.js
@@ -1,19 +1,27 @@
-fetch("data.php")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    showUser(data);
-  });
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
 
-  function showUser(User) {
-  const container = document.getElementById("User-list");
+    const cid = document.getElementById("cid").value;
+    const pass = document.getElementById("pass").value;
 
-  products.forEach(product => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-      <h3>${User.Cid}</h3>
-      <p>${product.Name}</p>
-    `;
-    container.appendChild(div);
-  });
-}
+    fetch("data.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            cid: cid,
+            password: pass
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("accounts", JSON.stringify(data.accounts));
+        localStorage.setItem("products", JSON.stringify(data.products));
+
+        window.location.href = "Hem.html";
+    })
+    .catch(err => console.error(err));
+});
