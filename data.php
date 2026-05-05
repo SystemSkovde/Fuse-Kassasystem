eec5d4763a02c5c6be3f3dfaf3aa2d5c35063571
Amanda-1111 <laisdaone@gmail.com>
2026-05-05 15:18:19 +0200

    test

--- data.php
+++ data.php
@@ -16,4 +16,15 @@ $stmt = $pdo->query($sql);
 $products = $stmt->fetchAll();
 
 echo json_encode($products);
+
+$data = json_decode(file_get_contents("php://input"), true);
+$cid = $data['cid'];
+$password = $data['password'];
+
+$sql2 = "SELECT * FROM Users where cid = :cid";
+$stmt = $pdo->prepare($sql2);
+$stmt->execute([':cid' => $cid]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+echo json_encode($user);
+
 ?>
