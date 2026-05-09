168c258e0dc9b70704395ab67ae4f593fb9c01df
dilyaraabdstudent-bit <dilyara.abd.student@gmail.com>
2026-05-09 13:30:27 +0200

    e
    e

--- dilara.php
+++ dilara.php
@@ -1,59 +1,42 @@
 <?php
-
-// Databas-anslutning
 $pdo = new PDO(
-    'mysql:dbname=Fuse;host=projekt.webug.se;charset=utf8mb4',
-    'dbftg',
-    'Myrlejon2026!',
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
+            'mysql:dbname=Fuse;host=projekt.webug.se;charset=utf8mb4',
+            'dbftg',
+            'Myrlejon2026!',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
 
 // Sätt attribut korrekt
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 
+$response = [];
 
-// Categories
-$sql = "SELECT * FROM categories";
+
+$sql = "SELECT * FROM Products";
 $stmt = $pdo->query($sql);
-$categories = $stmt->fetchAll();
+$response['products'] = $stmt->fetchAll();
 
 
+$cid = $_POST['cid'] ?? null;
+$password = $_POST['password'] ?? null;
 
-// Subcategories
-$sql = "SELECT * FROM subcategories";
-$stmt = $pdo->query($sql);
-$subcategories = $stmt->fetchAll();
+if ($cid !== null && $password !== null) {
+    $sql = "SELECT * FROM Users WHERE cid = :cid";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':cid' => $cid]);
 
-
-
-// Products
-$sql = "
-SELECT
-products.id,
-products.name,
-products.stock,
-products.category_id,
-products.subcategory_id,
-subcategories.name AS subcategory
-
-FROM products
-
-JOIN subcategories
-ON products.subcategory_id = subcategories.id
-";
-
-$stmt = $pdo->query($sql);
-$products = $stmt->fetchAll();
+    $response['user'] = $stmt->fetch();
 
 
+       $sql = "SELECT * FROM Accounts WHERE Cid = :cid";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':cid' => $cid]);
 
-// Returnera JSON
-echo json_encode([
-    "categories" => $categories,
-    "subcategories" => $subcategories,
-    "products" => $products
-]);
+    $response['accounts'] = $stmt->fetchAll();
+
+}
+echo json_encode($response);
 
 ?>
