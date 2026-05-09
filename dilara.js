9fad98da4954e1ae379ae4d3ed4eaa304870dcb3
dilyaraabdstudent-bit <dilyara.abd.student@gmail.com>
2026-05-09 13:32:02 +0200

    d
    d

--- dilara.js
+++ dilara.js
@@ -1,19 +1,63 @@
-fetch("dilara.php")
-  .then(response => response.json())
-  .then(data => {
+function docReady(fn) {
+if (document.readyState === "complete" || document.readyState === "interactive") {
+setTimeout(fn, 1);
+} else {
+document.addEventListener("DOMContentLoaded", fn);
+}
+}
 
-    allProducts = data.products;
-    allSubcategories = data.subcategories;
+docReady(function () {
+    document.querySelector("form")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
+   let products = {};
 
-    data.categories.forEach(category => {
+fetch("data.php")
+    .then(res => res.json())
+    .then(data => {
 
-      const option = document.createElement("option");
-
-      option.value = category.id;
-      option.textContent = category.name;
-
-      categorySelect.appendChild(option);
+        data.products.forEach(p => {
+           products[p.BarCode] = p;  
+        });
+        RenderProducts();
+        initScanner();
     });
 
-    showProducts(allProducts);
-  });
+  function RenderProducts() {
+        const IventoryDiv = document.getElementById("Inventory");
+
+        let html = `
+        <table class="cart-table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>SubCategory</th>
+                    <th>Stock</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+        `;
+
+        Object.values(products).forEach(item => {
+            html += `
+            <tr>
+                <td>${item.Name}</td> 
+                <td>${item.Category}</td> 
+                <td>${item.SubCategory}</td> 
+                <td>${item.Stock}</td> 
+                <td>${item.Price}</td> 
+            </tr>
+            `;
+        });
+
+        html += `
+            </tbody>
+        </table>`;
+
+        IventoryDiv.innerHTML = html;
+        updateCartCount();
+    }
+
+});
