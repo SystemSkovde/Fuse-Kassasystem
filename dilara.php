61ecfaf409bb53547bb870425ed96412d2df989c
dilyaraabdstudent-bit <dilyara.abd.student@gmail.com>
2026-05-09 14:33:09 +0200

    Update dilara.php

--- dilara.php
+++ dilara.php
@@ -29,6 +29,17 @@ if ($Category !== null && $SubCategory !== null) {
 
     $response['products'] = $stmt->fetchAll();
 }
+else if ($Category) {
+ ($Category !== null && $SubCategory !== null) {
+    $sql = "SELECT * FROM Products WHERE Category = :Category AND SubCategory = :SubCategory";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':Category' => $Category, ':SubCategory' => $SubCategory]);
+
+    $response['products'] = $stmt->fetchAll();
+
+
+
+    
 
 echo json_encode($response);
 
