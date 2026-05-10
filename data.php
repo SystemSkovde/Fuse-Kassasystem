4f73b394df1954d72c112608a808ff47af79af56
hedvigandersson99 <hedvig.andersson99@gmail.com>
2026-05-10 14:54:26 +0200

    Update data.php

--- data.php
+++ data.php
@@ -110,7 +110,7 @@ $stmt->execute($params);
 $response['products'] = $stmt->fetchAll();
 
 
-
+$data = json_decode(file_get_contents("php://input"), true);
 if ($data && isset($data['barcode'])) {
 
     $allowedFields = ['Name', 'Category', 'SubCategory', 'Stock', 'Price'];
